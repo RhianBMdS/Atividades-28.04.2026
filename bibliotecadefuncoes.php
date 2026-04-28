897c7a42899a911454c17cc5d8f548b96e21b966
Rhian B.M.S - 滅 <rhian.silva@edu.senai.br>
2026-04-28 10:39:52 -0300

    Atividade - 1, 2
    Adicionado o calculo da area do quadro, triangulo, retangulo, circulo e trapezio.

--- bibliotecadefuncoes.php
+++ bibliotecadefuncoes.php
@@ -1,27 +1,44 @@
 <?php
-//ATIVIDADE - 1
-namespace dolarParaReal;
+//ATIVIDADE - 1, 2
+namespace conversao;
 function converterD($valorD, $cotacaoR){
     return $valorD * $cotacaoR;}
     echo "O resultado da conversão de dólar para real é: ", converterD(2, 5);
 
-namespace euroParaReal;
 function converterE($valorE, $cotacaoR){
     return $valorE * $cotacaoR;}
     echo "\nO resultado da conversão de euro para real é: ", converterE(2, 5.85);
 
-    namespace pesoParaReal;
 function converterP($valorP, $cotacaoR){
     return $valorP * $cotacaoR;}
     echo "\nO resultado da conversão de peso para real é: ", converterP(2, 0.0035);
 
-    namespace libraParaReal;
 function converterL($valorL, $cotacaoR){
     return $valorL * $cotacaoR;}
     echo "\nO resultado da conversão de libra para real é: ", converterL(2, 6.74);
 
-    namespace ieneParaReal;
 function converterI($valorI, $cotacaoR){
     return $valorI * $cotacaoR;}
     echo "\nO resultado da conversão de iene para real é: ", converterI(2, 0.031);
+
+    namespace geometria;
+function areaQuadrado($lado){
+    return $lado * $lado;}
+    echo "\nA area do quadrado é: ", areaQuadrado(5);
+
+function areaRetangulo($base, $altura){
+    return $base * $altura;}
+    echo "\nA area do retangulo é: ", areaRetangulo(5, 10);
+
+function areaTriangulo($base, $altura){
+    return $base * $altura;}
+    echo "\nA area do triangulo é: ", areaTriangulo(5, 5);
+
+function areaCirculo($raio){
+    return 3.14 * ($raio * $raio);}
+    echo "\nA area do circulo é: ", areaCirculo(2);
+
+function areaTrapezio($baseMaior, $baseMenor, $altura){
+    return ($baseMaior + $baseMenor * $altura) / 2;}
+    echo "\nA area do trapezio é: ", areaTrapezio(5, 2 ,2);
 ?>
--- entradaDados.php
+++ entradaDados.php
@@ -1,13 +1,23 @@
 <?php
 require_once "bibliotecadefuncoes.php";
-use function dolarParaReal\converterD;
-use function euroParaReal\converterE;
-use function pesoParaReal\converterP;
-use function libraParaReal\converterL;
-use function ieneParaReal\converterI;
-echo converterD (1,1,1);
-echo converterE (1,1,1);
-echo converterP (1,1,1);
-echo converterL (1,1,1);
-echo converterI (1,1,1);
+use function conversao\converterD;
+use function conversao\converterE;
+use function conversao\converterP;
+use function conversao\converterL;
+use function conversao\converterI;
+use function geometria\areaQuadrado;
+use function geometria\areaRetangulo;
+use function geometria\areaTriangulo;
+use function geometria\areaCirculo;
+use function geometria\areaTrapezio;
+echo converterD (1,1);
+echo converterE (1,1);
+echo converterP (1,1);
+echo converterL (1,1);
+echo converterI (1,1);
+echo areaQuadrado (1);
+echo areaRetangulo (1,1);
+echo areaTriangulo (1,1);
+echo areaCirculo (1);
+echo areaTrapezio (1,1,1);
 ?>
