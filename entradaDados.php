9d1d00aab77e6ca633cade0fe16fecccb0f3580f
Rhian B.M.S - 滅 <rhian.silva@edu.senai.br>
2026-04-28 12:06:17 -0300

    Upload de segurança
    Upload de segurança em caso de troca de lugar

--- entradaDados.php
+++ entradaDados.php
@@ -1,30 +1,47 @@
 <?php
 require_once "bibliotecadefuncoes.php";
+
+//FUNÇÕES CONVERSORES
 use function conversao\converterD;
 use function conversao\converterE;
 use function conversao\converterP;
 use function conversao\converterL;
 use function conversao\converterI;
+
+//FUNÇÕES AREAS
+
 use function geometria\areaQuadrado;
 use function geometria\areaRetangulo;
 use function geometria\areaTriangulo;
 use function geometria\areaCirculo;
 use function geometria\areaTrapezio;
+
+//FUNÇÕES SAUDE
+
 use function saude\calcularimc;
 use function saude\valorIdealAgua;
 use function saude\frequenciaCardiacaMaxima;
 use function saude\converterLibrasParaQuilo;
 use function saude\calcularCaloriasBasais;
+
+//ECHOS CONVERSORES
+
 echo converterD (1,1);
 echo converterE (1,1);
 echo converterP (1,1);
 echo converterL (1,1);
 echo converterI (1,1);
+
+//ECHOS AREAS
+
 echo areaQuadrado (1);
 echo areaRetangulo (1,1);
 echo areaTriangulo (1,1);
 echo areaCirculo (1);
 echo areaTrapezio (1,1,1);
+
+//ECHOS AUDE
+
 echo calcularimc (1,1);
 echo valorIdealAgua (1);
 echo frequenciaCardiacaMaxima (1);
